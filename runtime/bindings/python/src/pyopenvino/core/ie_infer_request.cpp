f4e7e59f1168592c7e4ea5efa9edb09efffa963a
pszmel <piotr.szmelczynski@intel.com>
2021-10-11 11:34:39 +0200

    update InferRequest class

--- runtime/bindings/python/src/pyopenvino/core/ie_infer_request.cpp
+++ runtime/bindings/python/src/pyopenvino/core/ie_infer_request.cpp
@@ -5,6 +5,7 @@
 
 #include <ie_common.h>
 #include <pybind11/functional.h>
+#include <pybind11/stl_bind.h>
 
 #include <string>
 
@@ -14,6 +15,8 @@
 #include "pyopenvino/core/ie_preprocess_info.hpp"
 
 namespace py = pybind11;
+
+PYBIND11_MAKE_OPAQUE(std::vector<ov::runtime::ProfilingInfo>);
 
 void regclass_InferRequest(py::module m) {
     py::class_<InferRequestWrapper, std::shared_ptr<InferRequestWrapper>> cls(m, "InferRequest");
@@ -40,61 +43,75 @@ void regclass_InferRequest(py::module m) {
             }
             // Call Infer function
             self._startTime = Time::now();
-            self._request.Infer();
+            self._request.infer();
             self._endTime = Time::now();
             // Get output Blobs and return
             Containers::PyResults results;
             for (auto& out : self._outputsInfo) {
-                results[out.first] = self._request.GetBlob(out.first);
+                results[out.first] = self._request.get_tensor(out.first);
             }
             return results;
         },
         py::arg("inputs"));
 
     cls.def(
-        "_async_infer",
+        "start_async",
         [](InferRequestWrapper& self, const py::dict inputs, py::object userdata) {
             py::gil_scoped_release release;
             if (!inputs.empty()) {
                 Common::set_request_blobs(self._request, inputs);
             }
-            // TODO: check for None so next async infer userdata can be updated
-            // if (!userdata.empty())
-            // {
-            //     if (user_callback_defined)
-            //     {
-            //         self._request.SetCompletionCallback([self, userdata]() {
-            //             // py::gil_scoped_acquire acquire;
-            //             auto statusCode = const_cast<InferRequestWrapper&>(self).Wait(
-            //                 InferenceEngine::IInferRequest::WaitMode::STATUS_ONLY);
-            //             self._request.user_callback(self, statusCode, userdata);
-            //             // py::gil_scoped_release release;
-            //         });
-            //     }
-            //     else
-            //     {
-            //         py::print("There is no callback function!");
-            //     }
-            // }
+            TODO: check for None so next async infer userdata can be updated
+            if (!userdata.empty())
+            {
+                if (user_callback_defined)
+                {
+                    self._request.SetCompletionCallback([self, userdata]() {
+                        // py::gil_scoped_acquire acquire;
+                        auto statusCode = const_cast<InferRequestWrapper&>(self).Wait(
+                            InferenceEngine::IInferRequest::WaitMode::STATUS_ONLY);
+                        self._request.user_callback(self, statusCode, userdata);
+                        // py::gil_scoped_release release;
+                    });
+                }
+                else
+                {
+                    py::print("There is no callback function!");
+                }
+            }
             self._startTime = Time::now();
-            self._request.StartAsync();
+            self._request.start_async();
         },
         py::arg("inputs"),
         py::arg("userdata"));
 
     cls.def("cancel", [](InferRequestWrapper& self) {
-        self._request.Cancel();
+        self._request.cancel();
     });
 
     cls.def(
         "wait",
-        [](InferRequestWrapper& self, int64_t millis_timeout) {
+        [](InferRequestWrapper& self) {
             py::gil_scoped_release release;
-            return self._request.Wait(millis_timeout);
-        },
-        py::arg("millis_timeout") = InferenceEngine::IInferRequest::WaitMode::RESULT_READY);
+            return self._request.wait();
+        });
 
     cls.def(
+        "set_callback",
+        [](InferRequestWrapper& self, py::function f_callback) {
+            self._request.set_callback([&self, f_callback]() {
+                self._endTime = Time::now();
+                self._request.wait();
+
+                // Acquire GIL, execute Python function
+                py::gil_scoped_acquire acquire;
+                f_callback(self);
+            });
+        },
+        py::arg("f_callback"));
+
+
+        cls.def(
         "set_completion_callback",
         [](InferRequestWrapper& self, py::function f_callback, py::object userdata) {
             self._request.SetCompletionCallback([&self, f_callback, userdata]() {
@@ -113,36 +130,8 @@ void regclass_InferRequest(py::module m) {
         py::arg("userdata"));
 
     cls.def("get_profiling_info", [](InferRequestWrapper& self) {
-        std::map<std::string, InferenceEngine::InferenceEngineProfileInfo> perfMap;
-        perfMap = self._request.GetPerformanceCounts();
-        py::dict perf_map;
-        py::list prof_info;
-
-        for (auto it : perfMap) {
-            py::dict profile_info;
-            switch (it.second.status) {
-            case InferenceEngine::InferenceEngineProfileInfo::EXECUTED:
-                profile_info["status"] = "EXECUTED";
-                break;
-            case InferenceEngine::InferenceEngineProfileInfo::NOT_RUN:
-                profile_info["status"] = "NOT_RUN";
-                break;
-            case InferenceEngine::InferenceEngineProfileInfo::OPTIMIZED_OUT:
-                profile_info["status"] = "OPTIMIZED_OUT";
-                break;
-            default:
-                profile_info["status"] = "UNKNOWN";
-            }
-            profile_info["exec_type"] = it.second.exec_type;
-            profile_info["layer_type"] = it.second.layer_type;
-            profile_info["cpu_time"] = it.second.cpu_uSec;
-            profile_info["real_time"] = it.second.realTime_uSec;
-            profile_info["execution_index"] = it.second.execution_index;
-            perf_map[it.first.c_str()] = profile_info;
-
-            prof_info.append(it.second);
-        }
-        return perf_map;
+        const std::vector<ov::runtime::ProfilingInfo> prof_vec = self._request.get_profiling_info();
+        return prof_vec;
     });
 
     cls.def(
@@ -152,6 +141,17 @@ void regclass_InferRequest(py::module m) {
         },
         py::arg("name"));
 
+    cls.def("get_tensor", [](InferRequestWrapper& self, const std::string& name) {
+        self._request.get_tensor(name);
+        },
+        py::arg("name"));
+
+    cls.def("set_tensor", [](InferRequestWrapper& self, const std::string& name, const ov::runtime::Tensor& tensor) {
+        self._request.set_tensor(name, tensor);
+    },
+    py::arg("name"),
+    py::arg("tensor"));
+
     //    cls.def_property_readonly("preprocess_info", [](InferRequestWrapper& self) {
     //
     //    });
@@ -159,7 +159,7 @@ void regclass_InferRequest(py::module m) {
     cls.def_property_readonly("input_blobs", [](InferRequestWrapper& self) {
         Containers::PyResults input_blobs;
         for (auto& in : self._inputsInfo) {
-            input_blobs[in.first] = self._request.GetBlob(in.first);
+            input_blobs[in.first] = self._request.get_tensor(in.first);
         }
         return input_blobs;
     });
@@ -167,7 +167,7 @@ void regclass_InferRequest(py::module m) {
     cls.def_property_readonly("output_blobs", [](InferRequestWrapper& self) {
         Containers::PyResults output_blobs;
         for (auto& out : self._outputsInfo) {
-            output_blobs[out.first] = self._request.GetBlob(out.first);
+            output_blobs[out.first] = self._request.get_tensor(out.first);
         }
         return output_blobs;
     });
