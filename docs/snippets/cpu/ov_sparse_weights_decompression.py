557c4c525f50fa476a3ac2f30e3f6df714b83e61
jiwaszki <jan.iwaszkiewicz@intel.com>
2023-08-25 12:21:23 +0000

    snippets CPU 6/6

--- docs/snippets/cpu/ov_sparse_weights_decompression.py
+++ docs/snippets/cpu/ov_sparse_weights_decompression.py
@@ -2,10 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import openvino.runtime as ov
+import openvino as ov
 
-device_name = 'CPU'
-xml_path = 'model.xml'
+device_name = "CPU"
+xml_path = "model.xml"
 # ! [ov:intel_cpu:sparse_weights_decompression:part0]
 core = ov.Core()
 core.set_property("CPU", ov.properties.intel_cpu.sparse_weights_decompression_rate(0.8))
