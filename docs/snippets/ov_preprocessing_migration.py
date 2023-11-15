6fbdff6afe38ace19b537d1398fa271b8ea2edb0
Anastasia Kuporosova <anastasia.kuporosova@intel.com>
2023-11-15 14:21:02 +0000

    remove old api from python snippets

--- docs/snippets/ov_preprocessing_migration.py
+++ docs/snippets/ov_preprocessing_migration.py
@@ -2,18 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from utils import get_model, get_ngraph_model
+from utils import get_model
 
 #! [ov_imports]
 from openvino import Core, Layout, Type
 from openvino.preprocess import ColorFormat, PrePostProcessor, ResizeAlgorithm
 #! [ov_imports]
 
-#! [imports]
-import openvino.inference_engine as ie
-#! [imports]
-
-#include "inference_engine.hpp"
 
 tensor_name="input"
 core = Core()
@@ -64,39 +59,3 @@ input.preprocess().resize(ResizeAlgorithm.RESIZE_LINEAR, 448, 448)
 # and insert operations to the model
 model = ppp.build()
 #! [ov_image_scale]
-
-import openvino.inference_engine as ie
-import ngraph as ng
-
-operation_name = "data"
-
-core = ie.IECore()
-network = get_ngraph_model()
-
-
-#! [mean_scale]
-preprocess_info = network.input_info[operation_name].preprocess_info
-preprocess_info.init(3)
-preprocess_info[0].mean_value = 116.78
-preprocess_info[1].mean_value = 116.78
-preprocess_info[2].mean_value = 116.78
-preprocess_info[0].std_scale = 57.21
-preprocess_info[1].std_scale = 57.45
-preprocess_info[2].std_scale = 57.73
-preprocess_info.mean_variant = ie.MeanVariant.MEAN_VALUE
-#! [mean_scale]
-
-#! [conversions]
-input_info = network.input_info[operation_name]
-input_info.precision = "U8"
-input_info.layout = "NHWC"
-# model input layout is always NCHW in Inference Engine
-# for shapes with 4 dimensions
-#! [conversions]
-
-#! [image_scale]
-preprocess_info = network.input_info[operation_name].preprocess_info
-# Inference Engine supposes input for resize is always in NCHW layout
-# while for OpenVINO Runtime API 2.0 `H` and `W` dimensions must be specified
-# Also, current code snippet supposed resize from dynamic shapes
-preprocess_info.resize_algorithm = ie.ResizeAlgorithm.RESIZE_BILINEAR
